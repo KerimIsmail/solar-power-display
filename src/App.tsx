b5bd5e1cad53cc1782e50b9412b95c65118de138
KerimIsmail <Kerim.Ismail.Oglou@Pfreundt.de>
2024-11-27 09:06:26 +0100

    Added Axios + example

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
     <>
       <h1>Solar Power Display</h1>
-      
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
