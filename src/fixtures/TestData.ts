02461ddfcab20a6ed7858a9aa1a6e2299a985525
KerimIsmail <Kerim.Ismail.Oglou@Pfreundt.de>
2024-11-29 10:25:28 +0100

    Added Test data

---
+++ src/fixtures/TestData.ts
@@ -0,0 +1,10 @@
+import { BackendData } from "@/types/BackendData";
+
+export const TEST_DATA: BackendData = {
+  daily: "12020",
+  total: "10000",
+  measurements: [
+    {timestamp: "123", currentEnergy: "100"},
+    {timestamp: "2344", currentEnergy: "100"},
+  ],
+};
